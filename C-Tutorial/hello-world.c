734c0525deb7e19507586c3a859e3ab78b87e786
CheeseLad <cheesescreativeserver@gmail.com>
2023-08-19 14:41:56 +0100

    initial upload

---
+++ C-Tutorial/hello-world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int hello_world()
+{
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ C-Tutorial/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hey George!\n");
+    printf("He is 70\n");
+    printf("He likes the name George\n");
+    printf("But he does not like being 70\n");
+    return 0;
+}
